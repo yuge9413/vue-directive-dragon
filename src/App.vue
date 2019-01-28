d40702833ac04e317ec050ec3e0d1f7f835221e1
yuge9413 <282729573@qq.com>
2019-01-28 18:19:41 +0800

    feat: 开发简单例子

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,24 @@
 <template>
     <div id="app">
-        {{ text }}
+        <div
+            class="box"
+            v-dragon="{target: '.head'}"
+        >
+            <div class="head">
+                test
+            </div>
+            xxxxxxx
+            xxxxxxx
+        </div>
     </div>
 </template>
 
 <script>
+import Vue from 'vue';
+import dragon from './directive/dragon';
+
+Vue.use(dragon);
+
 export default {
     name: 'App',
     data() {
@@ -23,5 +37,16 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.box {
+    width: 400px;
+    height: 200px;
+    background: #eee;
+}
+
+.head {
+    height: 50px;
+    line-height: 50px;
 }
 </style>
