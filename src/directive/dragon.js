1ba817c8c9a6a34e2fdbfd998390fab2f3d3721c
平凡公子 <282729573@qq.com>
2020-04-25 16:17:45 +0800

    fix: 去除无用方法

--- src/directive/dragon.js
+++ src/directive/dragon.js
@@ -221,36 +221,6 @@ const utils = {
             document.getElementsByTagName('head')[0].appendChild(style);
         }
     },
-
-    /**
-     * Debounce 处理函数
-     *
-     * @param {Function} fn 实际要执行的函数
-     * @param {number} delay  延迟时间，单位是毫秒（ms）
-     * @return {Function} 返回一个“防反跳”了的函数
-     */
-    debounce(fn, delay) {
-        let timer;
-
-        // 返回一个函数，这个函数会在一个时间区间结束后的 delay 毫秒时执行 fn 函数
-        // eslint-disable-next-line
-        return function () {
-            // 保存函数调用时的上下文和参数，传递给 fn
-            // eslint-disable-next-line
-            const context = this;
-            // eslint-disable-next-line
-            const args = arguments;
-
-            // 每次这个返回的函数被调用，就清除定时器，以保证不执行 fn
-            clearTimeout(timer);
-
-            // 当返回的函数被最后一次调用后（也就是用户停止了某个连续的操作），
-            // 再过 delay 毫秒就执行 fn
-            timer = setTimeout(() => {
-                fn.apply(context, args);
-            }, delay);
-        };
-    },
 };
 
 /**
@@ -532,10 +502,11 @@ class Dragon {
             'user-select': '',
         };
 
+        // 鼠标事件 - 增加引用 用于解绑事件
         this.mousedownFn = e => this.startDrag(e);
-
+        // 鼠标事件 - 增加引用 用于解绑事件
         this.mousemoveFn = e => this.drag(e);
-
+        // 鼠标事件 - 增加引用 用于解绑事件
         this.mouseupFn = e => this.endDrag(e);
 
         this.init();
